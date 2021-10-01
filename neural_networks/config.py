7d3f79076dd070154add50ba39f689fd02a25bec
Kelvin Ducray <kelvinducray@192-168-1-101.tpgi.com.au>
2021-10-01 22:16:44 +1000

    Hello world MLP model working and added more to project structure

--- neural_networks/config.py
+++ neural_networks/config.py
@@ -3,11 +3,15 @@ from typing import Union
 from pydantic import BaseSettings
 
 
-class FullyConnectedSettings(BaseSettings):
-    BATCH_SIZE: int = 64
-
-    DATA_USE_SUBSET: bool = True
+class ProjectBaseSettings(BaseSettings):
     DATA_DIR: str = "./data/"
+
+    GPU: str = ""  # TODO - should this be dynamic?
+
+
+class FullyConnectedSettings(ProjectBaseSettings):
+    BATCH_SIZE: int = 64
+    DATA_USE_SUBSET: bool = True
 
 
 # For type checking purposes:
--- neural_networks/pl_modules/fully_connected.py
+++ neural_networks/pl_modules/fully_connected.py
@@ -1,32 +1,32 @@
 import pytorch_lightning as pl
 import torch
-import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss, Flatten
 
-from ..models.fully_connected import FullyConnected
+from ..models.fully_connected import init_fully_connected
 
 
 class FullyConnectedModule(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.network = FullyConnected()
+        self.flatten = Flatten()
+        self.network = init_fully_connected()
+        self.criterion = CrossEntropyLoss()
 
     def forward(self, x):
-        # in lightning, forward defines the prediction/inference actions
-        embedding = self.encoder(x)
-        return embedding
+        x = self.flatten(x)
+        return self.network(x)
 
     def training_step(self, batch, batch_idx):
-        # training_step defined the train loop.
-        # It is independent of forward
+        # training_step defined the train loop - it is independent of forward
         x, y = batch
         x = x.view(x.size(0), -1)
-        z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, x)
+        y_hat = self.network(x)
+        loss = self.criterion(y_hat, y)
+
         # Logging to TensorBoard by default
         self.log("train_loss", loss)
+
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+        return torch.optim.SGD(self.parameters(), lr=1e-3)
